cdfdca3f80af24b3731a091950d037aac3db1bad
Ihebmsaed <158514133+Ihebmsaed@users.noreply.github.com>
2025-02-06 19:57:16 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,12 @@
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      clearContext: false, // Garde les résultats des tests dans le navigateur
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'], // Utiliser ChromeHeadless
+    browsers: ['FirefoxHeadless'],
     customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222',
-          '--disable-dev-shm-usage',
-        ],
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless'],
       },
     },
-    singleRun: false, // Mettez à true pour exécuter les tests une seule fois en CI
-    restartOnFileChange: true,
+    // Other configurations...
   });
 };
